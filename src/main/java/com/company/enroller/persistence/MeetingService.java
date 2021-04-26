3fa700bf5163e370a9c125c17d5a65444af53bfb
majawilczynska <maja.wilczynska@gmail.com>
2021-04-26 04:47:37 +0200

    MeetingService

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -7,6 +7,7 @@ import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
 
 @Component("meetingService")
 public class MeetingService {
@@ -21,6 +22,45 @@ public class MeetingService {
 		String hql = "FROM Meeting";
 		Query query = connector.getSession().createQuery(hql);
 		return query.list();
+	}
+
+	public Meeting findById(long id) {
+		Meeting meeting = (Meeting) connector.getSession().get(Meeting.class, id);
+		return meeting;
+	}
+
+	public void add(Meeting meeting) {
+		Transaction transaction = this.connector.getSession().beginTransaction();
+		connector.getSession().save(meeting);
+		transaction.commit();
+	}
+
+	public void delete(Meeting meeting) {
+		Transaction transaction = this.connector.getSession().beginTransaction();
+		connector.getSession().delete(meeting);
+		transaction.commit();
+	}
+
+	public void update(Meeting meeting) {
+		Transaction transaction = this.connector.getSession().beginTransaction();
+		connector.getSession().merge(meeting);
+		transaction.commit();
+	}
+
+	public Collection<Participant> getByIdMeeting(long id) {
+		Meeting meeting = (Meeting) connector.getSession().get(Meeting.class, id);
+		Collection<Participant> participants = meeting.getParticipants();
+		return participants;
+	}
+
+	public void addParticipantToMeeting(long id, String login) {
+		Meeting meeting = (Meeting) connector.getSession().get(Meeting.class, id);
+		Participant participant = (Participant) connector.getSession().get(Participant.class, login);
+		meeting.addParticipant(participant);
+		Transaction transaction = this.connector.getSession().beginTransaction();
+		connector.getSession().save(meeting);
+		transaction.commit();
+
 	}
 
 }
